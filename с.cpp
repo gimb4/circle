6d7bad0773cf01ec1389d85c8fd6bea07a9cbe83
rr <gregrger@gmail.com>
2023-11-08 15:42:05 +0300

    cyclone

---
+++ с.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int radius = 6; 
+    int centerX = 10; 
+    int centerY = 10; 
+
+    for (int y = 0; y <= 2 * radius; ++y) {
+        for (int x = 0; x <= 2 * radius; ++x) {
+            int dx = x - radius;
+            int dy = y - radius;
+            if (dx * dx + dy * dy <= radius * radius) {
+                cout << "* ";
+            }
+            else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
